c35884ea0029a62c42a3936b2c8724a2de6d244f
Gabriel <biel_dmarco@hotmail.com>
2020-03-10 20:59:27 -0300

    add mongodb config and connection

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.port || 3000;
 const router = express.Router();
 
-app.use(bodyParser.urlencoded({extended:true}));
+// DB config
+mongoose.connect("mongodb://localhost/bdCrud", { useNewUrlParser: true, useUnifiedTopology: true });
+
+// App config
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-router.use(function(req,res,next){
-    console.log("Intercepted by Middlware!");
-    next();
+// Middlewares
+router.use(function(req, res, next) {
+  console.log("Intercepted by Middlware!");
+  next();
 });
 
-router.get('/', (req,res) => {
-    res.json({"message": "Test route ok!"});
+// Routes
+router.get("/", (req, res) => {
+  res.json({ message: "Test route ok!" });
 });
 
-app.use('/api', router);
+// Config router
+app.use("/api", router);
+
+// Start app
 app.listen(PORT, () => {
-    console.log("server is up and running");
+  console.log("server is up and running");
 });
