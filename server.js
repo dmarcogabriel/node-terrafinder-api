0083b6c2fcff3ce5bfcf937bdfdcc33196734cd8
dmarcogabriel <biel_dmarco@hotmail.com>
2020-03-24 23:26:33 +0000

    add conection to mongodb cloud and dotenv config

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+require("dotenv/config");
+
+console.log(process.env.DB_USER)
 
 const app = express();
 const PORT = process.env.port || 3000;
 const router = express.Router();
 
 // DB config
-mongoose.connect("mongodb://localhost/bdCrud", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-bkb6b.mongodb.net/test?retryWrites=true&w=majority`, 
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 // App config
 app.use(bodyParser.urlencoded({ extended: true }));
