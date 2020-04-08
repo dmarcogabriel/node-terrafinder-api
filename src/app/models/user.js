0dbc7be3c08a7d7eab2639a0df90e2f15a6b2a2d
dmarcogabriel <biel_dmarco@hotmail.com>
2020-04-08 02:02:01 +0000

    update user model exports

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require('mongoose')
 
-exports.User = model('User', new Schema({
+module.exports = model('User', new Schema({
   name: String,
   email: String,
   password: String
