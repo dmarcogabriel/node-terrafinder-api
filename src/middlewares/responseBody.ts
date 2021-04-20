e99a7bf830c7da0c234bba586ad18c22b049ad08
Gabriel De Marco <biel_dmarco@hotmail.com>
2021-04-20 20:03:18 -0300

    add middlewares layer

---
+++ src/middlewares/responseBody.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from 'express'
+import { ResponseBody } from '../interfaces/ResponseBody'
+
+export const logger = () => (
+  req: Request,
+  res: Response<ResponseBody>,
+  next: NextFunction,
+): void => {
+  // todo: add logs here
+  next()
+}
