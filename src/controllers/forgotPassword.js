e32e9c97735713d677080e035ba33a5d203db7f1
Gabriel De Marco <biel_dmarco@hotmail.com>
2021-04-20 20:05:18 -0300

    update controllers to typescript

--- src/controllers/forgotPassword.js
+++
@@ -1,7 +0,0 @@
-// todo: add send email feature
-exports.sendForgotPasswordEmail = async (_, res) => res.status(200)
-  .json({ message: 'Um e-mail foi enviado para sua caixa de email.' })
-
-// todo: add a reset password feature
-exports.resetPassword = async (_, res) => res.status(200)
-  .json({ message: 'Senha alterada com sucesso!' })
---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from 'express'
+import { isArray } from 'lodash'
+import repository from '../repositories/user'
+import { ResponseBody } from '../interfaces/ResponseBody'
+
+const get = async (_: Request, res: Response<ResponseBody>): Promise<void> => {
+  try {
+    const users = await repository.getAll()
+    res.status(200).json({ message: 'OK', data: { users } })
+  } catch (error) {
+    res.status(500).json({ message: 'not ok', data: { error } })
+  }
+}
+
+const post = async (req: Request, res: Response<ResponseBody>): Promise<void> => {
+  try {
+    const userId = await repository.create(req.body)
+    res.status(201).json({
+      message: 'User created successfully.',
+      data: { userId },
+    })
+  } catch (error) {
+    res.status(500).json({ message: 'not ok', data: { error } })
+  }
+}
+
+const getById = async (req: Request, res: Response<ResponseBody>): Promise<void> => {
+  try {
+    const user = await repository.getAll(req.params.id)
+    res.status(200).json({ message: 'ok', data: { user } })
+  } catch (error) {
+    res.status(500).json({ message: 'not ok', data: { error } })
+  }
+}
+
+const uploadFile = async (req: Request, res: Response<ResponseBody>): Promise<void> => {
+  if (!req.files && !req.params.id) {
+    res.status(400).json({ message: 'Nenhuma imagem foi selecionada' })
+  } else {
+    try {
+      const { params, files } = req
+      const file = isArray(files.avatar) ? files.avatar[0] : files.avatar
+      const avatar = await repository.updateAvatar(file, params.id)
+
+      res.status(201).json({
+        message: 'Imagem adicionada com sucesso',
+        data: { avatar },
+      })
+    } catch (error) {
+      res.status(500).json({
+        message: 'Ocorreu um erro ao fazer upload',
+        data: { error },
+      })
+    }
+  }
+}
+
+export default {
+  get, getById, post, uploadFile,
+}
